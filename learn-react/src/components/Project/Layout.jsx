68671bdb6ca04624664e65072184ca618682fe17
Ravi Parmar <ravi.parmar@simulas.in>
2025-01-27 10:15:20 +0000

    theme changer

--- learn-react/src/components/Project/Layout.jsx
+++ learn-react/src/components/Project/Layout.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import { Outlet } from "react-router-dom";
 import UserContextProvider from "../../Contexts/UserContextProvider";
 import Login from "../ContextComponents/Login";
 import Profile from "../ContextComponents/Profile";
+import { ThemeProvider } from "../../Contexts/ThemeContext";
+import ThemeBtn from "../ThemeComponents/ThemeBtn";
+import Card from "../ThemeComponents/Card";
 
 function Layout() {
+  const [themeMode, setThemeMode] = useState("light");
+  const toggleTheme = () => {
+    setThemeMode((prev) => (prev == "light" ? "dark" : "light"));
+  };
+  useEffect(() => {
+    const classList = document.querySelector("html").classList;
+    classList.remove("light", "dark");
+    classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <>
       <Header />
@@ -16,6 +29,20 @@ function Layout() {
         <Login />
         <Profile />
       </UserContextProvider>
+
+      <ThemeProvider value={{ themeMode, toggleTheme }}>
+        <div className="flex flex-wrap min-h-screen items-center">
+          <div className="w-full">
+            <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+              <ThemeBtn />
+            </div>
+
+            <div className="w-full max-w-sm mx-auto">
+              <Card />
+            </div>
+          </div>
+        </div>
+      </ThemeProvider>
     </>
   );
 }
